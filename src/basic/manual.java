b189571ee33eff38b5ed1a897ebf357658c64b35
rashed2019 <hasan.rashidul80usa@gmail.com>
2022-12-25 14:23:50 -0500

    new line added

--- src/basic/manual.java
+++ src/basic/manual.java
@@ -2,7 +2,7 @@ package basic;
 
 public class manual {
 public static void main(String []args){
-    System.out.println("we the student of summer 2022");
+    System.out.println("we the student of summer 2023");
 
 }
 
